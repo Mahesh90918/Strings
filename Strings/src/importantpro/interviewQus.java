89c5d4f94c8bac340ffd975182e819902e4783f0
Mahesh90918 <112085942+Mahesh90918@users.noreply.github.com>
2022-11-25 10:58:27 +0530

    imp

---
+++ Strings/src/importantpro/interviewQus.java
@@ -0,0 +1,5 @@
+    System.out.println("mahesh"+10+20);//mahesh1020
+		System.out.println(20+30+"mahesh"+10+20);//50 mahesh 1020
+		System.out.println(20+30+"mahesh".concat("10").concat("20"));
+		System.out.println(20+30+"mahesh"+(10+20));// 50 mahesh 30
+		System.out.println(20+30+"mahesh");//50mahesh
